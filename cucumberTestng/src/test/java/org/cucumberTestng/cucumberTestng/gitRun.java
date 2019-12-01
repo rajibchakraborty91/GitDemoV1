eae4de0f46a790524a5cafc16fae1360124628d2
Rajib <Rajib@DESKTOP-MSUCRPV>
2019-12-01 20:39:51 +0530

    second class commit

---
+++ cucumberTestng/src/test/java/org/cucumberTestng/cucumberTestng/gitRun.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.cucumberTestng.cucumberTestng;
+
+/**
+ * @author Rajib
+ *
+ */
+public class gitRun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hey google");
+
+	}
+
+}
