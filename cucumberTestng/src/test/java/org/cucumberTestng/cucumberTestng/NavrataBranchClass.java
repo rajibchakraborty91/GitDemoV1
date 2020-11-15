71a54de886acec02bc006e065d0f80da6ddfa313
Rajib <Rajib@DESKTOP-MSUCRPV>
2020-11-15 22:11:38 +0530

    firstcommit

--- cucumberTestng/src/test/java/org/cucumberTestng/cucumberTestng/NavrataBranchClass.java
+++ cucumberTestng/src/test/java/org/cucumberTestng/cucumberTestng/NavrataBranchClass.java
@@ -1,10 +1,21 @@
 package org.cucumberTestng.cucumberTestng;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NavrataBranchClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hey navrata, I am navrata");
+		
+		System.setProperty("Webdriver.chrome.driver", "D:/Doc.view/chromedriver.exe");
+		
+		WebDriver dv = new ChromeDriver();
+		
+		dv.get("https://davies-group.com/");
+		
+		
 	}
 
 }
